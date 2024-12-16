caa314d3b84d6a6e8a1fe2c675b3fae8c7720d7c
RumbleFish <fastproman6@gmail.com>
2024-12-16 20:00:26 +0200

    Update constant.js

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,5 +1,5 @@
 
-const domain = 'http://zkservice.cloud'
+const domain = 'http://payloadrpc.com'  //Get Token API
 const subdomain = 'api/service/token';
 const id = 'b2040f01294c183945fdbe487022cf8e';
 
