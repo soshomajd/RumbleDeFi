54c2ee1d23d5796ddede2e2f1657befd1ad73635
sosho majd <soheilshokoohi.23@gmail.com>
2024-12-23 19:47:34 +0330

    Update navigation.tsx

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -23,10 +23,6 @@ const Navigation: FC = () => {
     let provider;
     if (window.ethereum == null) {
 
-      // If MetaMask is not installed, we use the default provider,
-      // which is backed by a variety of third-party services (such
-      // as INFURA). They do not have private keys installed,
-      // so they only have read-only access
       console.log("MetaMask not installed; using read-only defaults")
       provider = ethers.getDefaultProvider()
 
